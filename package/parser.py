3bb0392e7ef1a033fccc88984d250f4a71c60705
artemiy6451 <53995940+artemiy6451@users.noreply.github.com>
2020-06-13 15:00:24 +0300

    Update parser.py

--- package/parser.py
+++ package/parser.py
@@ -17,7 +17,7 @@ def get_html(url,params=None):
     return r
 
 def get_content(html):
-    soup = BeautifulSoup(html, 'html')
+    soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('span', class_="games_list_tab_number")
 
     print(items)
