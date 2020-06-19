e6fd993b34f791352cb010f9cb1168c32a66f44b
artemiy6451 <53995940+artemiy6451@users.noreply.github.com>
2020-06-19 16:44:28 +0300

    Add files via upload

--- package/parser.py
+++ package/parser.py
@@ -1,34 +1,63 @@
 import requests
 from bs4 import BeautifulSoup
+import steam.webauth as wa
+import steam.client.user
 
 
-baseId = 440_2_8815869294
 
 URL = 'https://steamcommunity.com/profiles/76561198373547526/inventory/#440'
+URL_2 = 'https://steamcommunity.com/profiles/76561198373547526/inventory/#440'
 Headers = {
     'Accept': '*/*',
     'Accept-Language': 'ru,en;q=0.9',
     'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 YaBrowser/20.4.3.257 Yowser/2.5 Yptp/1.23 Safari/537.36'
 }
 listings = []
+balance = []
+itemPrice = []
+
+def outh():
+    user = wa.WebAuth('emmett66666')
+    twofactor = input('Введите 2FA код: ')
+    session = user.login(password='223123ar', twofactor_code=twofactor)
+    return session
+
+session = outh()
 
 def get_html(url,params=None):
-    r = requests.get(url, params=params, headers=Headers)
+    r = session.get(url, params=params, headers=Headers)
+    print(r)
     return r
 
 def get_content(html):
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'lxml')
     items = soup.find_all('span', class_="games_list_tab_number")
+    bal = soup.find_all('a', class_='global_action_link', id='header_wallet_balance')
+    for b in bal:
+        balance.append({
 
-    print(items)
+            'balance' : b.get_text(strip=True)
+
+        })
+
     for i in items:
         listings.append({
 
             'count' : i.get_text(strip=True).replace('(','').replace(')','')
+
         })
     del listings[0:-2]
+    del balance[0:-2]
+    print(balance)
     print(listings)
-    return listings
+    return balance, listings
+
+def get_price(url):
+    r = session.get(url, headers=Headers)
+    html = r
+    soup = BeautifulSoup(html.text, 'lxml')
+    print(soup)
+
 
 def parse():
     html = get_html(URL)
@@ -64,6 +93,7 @@ def comparison():
 
         else:
             count_items = int(listings[-1].get('count'))
+            balance[-1].get('balance')
             print(count_items)
             print('1')
             return 1
